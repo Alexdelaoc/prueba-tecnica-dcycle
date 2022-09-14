0870072c2902118f17eddb18135771d7254e499d
Alexdelaoc <alexdelaoc@gmail.com>
2022-09-14 21:22:16 +0200

    [ADD] compare functionality

--- src/components/Main/Prueba2/Prueba2.jsx
+++ src/components/Main/Prueba2/Prueba2.jsx
@@ -5,6 +5,7 @@ import loadingGif from "../../../assets/loading.gif"
 const Prueba2 = () => {
 
   const [info, setInfo] = useState([]);
+  const [date, setDate] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -25,12 +26,22 @@ const Prueba2 = () => {
 
   const addThousandsDots = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-  }
+  };
+
+  // eslint-disable-next-line
+  let index = info.findIndex(item => item.date == date);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(index)
+    setDate(event.target.name.value);
+  };
 
   return (
     <main className="prueba2__main">
       <h1 id="prueba2__main-title">The COVID Tracking Project</h1>
-      {/* <h3 id="prueba2__main-section-date">Data for {info[0].date} at the US</h3> */}
+      {loading === false ? <h3 id="prueba2__main-section-date">Data for {info[0].date} at the US</h3> : ""}
       <p>Not receiving new data since March 7, 2021</p>
       {loading === false
         ? <section id="prueba2__main-section">
@@ -57,6 +68,37 @@ const Prueba2 = () => {
           </section>
         : <img src={loadingGif} className="loading-spinner" alt="spinner" />
       }
+
+      <form onSubmit={handleSubmit}>
+        <label>Compare current data with selected day data</label>
+        <input type="date" name="name" max="2021-03-07"/>
+        <button type="submit">Search</button>
+      </form>
+
+      {date
+      ? <section id="prueba2__main-section">
+          <article className="prueba2__main-section-box">
+            <h4>Cases</h4>
+            <h5>Total cases: {addThousandsDots(info[index].cases.total.value)}</h5>
+            <p>New cases today: {addThousandsDots(info[index].cases.total.calculated.change_from_prior_day)}</p>
+            <p>Change over 7 days {info[index].cases.total.calculated.seven_day_change_percent}%</p>
+          </article>
+
+          <article className="prueba2__main-section-box">
+            <h4>Tests</h4>
+            <h5>Total tests: {addThousandsDots(info[index].testing.total.value)}</h5>
+            <p>New tests today: {addThousandsDots(info[index].testing.total.calculated.change_from_prior_day)}</p>
+            <p>Change over 7 days {info[index].testing.total.calculated.seven_day_change_percent}%</p>
+          </article>
+
+          <article className="prueba2__main-section-box">
+            <h4>Outcomes</h4>
+            <h5>Deaths: {addThousandsDots(info[index].outcomes.death.total.value)}</h5>
+            <p>Population percent: {info[index].outcomes.death.total.calculated.population_percent}%</p>
+            <p>Last day deaths: {addThousandsDots(info[index].outcomes.death.total.calculated.change_from_prior_day)} people</p>
+          </article>
+        </section>
+      : ""}
     </main>
   )
 };
