84e23d8194a6a956d20396df5e12fc0b9af9b5f6
Alexdelaoc <alexdelaoc@gmail.com>
2022-09-15 02:15:27 +0200

    [FIX] refactorized Prueba2

--- src/components/Main/Prueba2/Prueba2.jsx
+++ src/components/Main/Prueba2/Prueba2.jsx
@@ -41,72 +41,71 @@ const Prueba2 = () => {
   };
 
   return (
-    <section className="prueba2__main">
-      <h1 id="prueba2__main-title">The COVID Tracking Project</h1>
+    loading === false
+      ? <section className="prueba2__main">
+        <h1 id="prueba2__main-title">The COVID Tracking Project</h1>
 
-      {loading === false ? <h3 id="prueba2__main-section-date">Data for {info[0].date} at the US</h3> : ""}
-      
-      {loading === false ? <p>Not receiving new data since March 7, 2021</p> : ""}
-      
-      {loading === false
-        ? <section id="prueba2__main-section">
-            <article className="prueba2__main-section-box">
-              <h4>Cases</h4>
-              <h5>Total cases (Accumulative): {addThousandsDots(info[0].cases.total.value)}</h5>
-              <p>New cases today: {addThousandsDots(info[0].cases.total.calculated.change_from_prior_day)}</p>
-              <p>Change within the previous 7 days: {info[0].cases.total.calculated.seven_day_change_percent}%</p>
-            </article>
+        <h3 id="prueba2__main-section-date">Data for {info[0].date} at the US</h3>
 
-            <article className="prueba2__main-section-box">
-              <h4>Tests</h4>
-              <h5>Total tests (Accumulative): {addThousandsDots(info[0].testing.total.value)}</h5>
-              <p>New tests today: {addThousandsDots(info[0].testing.total.calculated.change_from_prior_day)}</p>
-              <p>Change within the previous 7 days: {info[0].testing.total.calculated.seven_day_change_percent}%</p>
-            </article>
+        <p>Not receiving new data since March 7, 2021</p>
 
-            <article className="prueba2__main-section-box">
-              <h4>Outcomes</h4>
-              <h5>Deaths: {addThousandsDots(info[0].outcomes.death.total.value)}</h5>
-              <p>Population percent: {info[0].outcomes.death.total.calculated.population_percent}%</p>
-              <p>Last day deaths: {addThousandsDots(info[0].outcomes.death.total.calculated.change_from_prior_day)} people</p>
-            </article>
-          </section>
-        : <img src={loadingGif} className="loading-spinner" alt="spinner" />
-      }
-
-      {loading === false
-      ? <form onSubmit={handleSubmit}>
-          <label>Compare current data with selected day data</label>
-          <input type="date" name="name" max="2021-03-07" min="2020-01-13"/>
-          <button type="submit">Search</button>
-        </form>
-      : ""}
-
-      {date
-      ? <section id="prueba2__main-section">
+        <section id="prueba2__main-section">
           <article className="prueba2__main-section-box">
-            <h4>Cases ({info[index].date})</h4>
-            <h5>Total cases (Accumulative): {addThousandsDots(info[index].cases.total.value)}</h5>
-            <p>New cases on that day: {addThousandsDots(info[index].cases.total.calculated.change_from_prior_day)}</p>
-            <p>Change within the previous 7 days: {info[index].cases.total.calculated.seven_day_change_percent}%</p>
+            <h4>Cases</h4>
+            <h5>Total cases (Accumulative): {addThousandsDots(info[0].cases.total.value)}</h5>
+            <p>New cases today: {addThousandsDots(info[0].cases.total.calculated.change_from_prior_day)}</p>
+            <p>Change within the previous 7 days: {info[0].cases.total.calculated.seven_day_change_percent}%</p>
           </article>
 
           <article className="prueba2__main-section-box">
-            <h4>Tests ({info[index].date})</h4>
-            <h5>Total tests (Accumulative): {addThousandsDots(info[index].testing.total.value)}</h5>
-            <p>New tests on that day: {addThousandsDots(info[index].testing.total.calculated.change_from_prior_day)}</p>
-            <p>Change within the previous 7 days: {info[index].testing.total.calculated.seven_day_change_percent}%</p>
+            <h4>Tests</h4>
+            <h5>Total tests (Accumulative): {addThousandsDots(info[0].testing.total.value)}</h5>
+            <p>New tests today: {addThousandsDots(info[0].testing.total.calculated.change_from_prior_day)}</p>
+            <p>Change within the previous 7 days: {info[0].testing.total.calculated.seven_day_change_percent}%</p>
           </article>
 
           <article className="prueba2__main-section-box">
-            <h4>Outcomes at ({info[index].date})</h4>
-            <h5>Deaths: {addThousandsDots(info[index].outcomes.death.total.value)}</h5>
-            <p>Population percent: {info[index].outcomes.death.total.calculated.population_percent}%</p>
-            <p>Last day deaths: {addThousandsDots(info[index].outcomes.death.total.calculated.change_from_prior_day)} people</p>
+            <h4>Outcomes</h4>
+            <h5>Deaths: {addThousandsDots(info[0].outcomes.death.total.value)}</h5>
+            <p>Population percent: {info[0].outcomes.death.total.calculated.population_percent}%</p>
+            <p>Last day deaths: {addThousandsDots(info[0].outcomes.death.total.calculated.change_from_prior_day)} people</p>
           </article>
         </section>
-      : ""}
-    </section>
+
+
+        <form onSubmit={handleSubmit}>
+          <label>Compare current data with selected day data</label>
+          <input type="date" name="name" max="2021-03-07" min="2020-01-13" />
+          <button type="submit">Search</button>
+        </form>
+
+
+        {date
+          ? <section id="prueba2__main-section">
+            <article className="prueba2__main-section-box">
+              <h4>Cases ({info[index].date})</h4>
+              <h5>Total cases (Accumulative): {addThousandsDots(info[index].cases.total.value)}</h5>
+              <p>New cases on that day: {addThousandsDots(info[index].cases.total.calculated.change_from_prior_day)}</p>
+              <p>Change within the previous 7 days: {info[index].cases.total.calculated.seven_day_change_percent}%</p>
+            </article>
+
+            <article className="prueba2__main-section-box">
+              <h4>Tests ({info[index].date})</h4>
+              <h5>Total tests (Accumulative): {addThousandsDots(info[index].testing.total.value)}</h5>
+              <p>New tests on that day: {addThousandsDots(info[index].testing.total.calculated.change_from_prior_day)}</p>
+              <p>Change within the previous 7 days: {info[index].testing.total.calculated.seven_day_change_percent}%</p>
+            </article>
+
+            <article className="prueba2__main-section-box">
+              <h4>Outcomes at ({info[index].date})</h4>
+              <h5>Deaths: {addThousandsDots(info[index].outcomes.death.total.value)}</h5>
+              <p>Population percent: {info[index].outcomes.death.total.calculated.population_percent}%</p>
+              <p>Last day deaths: {addThousandsDots(info[index].outcomes.death.total.calculated.change_from_prior_day)} people</p>
+            </article>
+          </section>
+          : ""}
+      </section>
+      : <img src={loadingGif} className="loading-spinner" alt="spinner" />
   )
 };
 
